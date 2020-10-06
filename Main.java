d8e2a9c7126e8c7fd747f033c34d94ce75a40a28
blisn8405 <blisn8405@wrdsb.ca>
2020-10-06 15:28:12 +0000

    q3

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner; 
 /**
  *
  * @author 
@@ -9,7 +10,18 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a scanner for user input
+Scanner input = new Scanner(System.in);
+    //get users number
+System.out.println("Please enter a positive integer");
+   int number = input.nextInt(); 
+   //create another variable
+   int answer = 1; 
+       // a for loop
+for(int i = number; i >=1; i--){
+    answer= answer * i;
+}
+ System.out.println(number + "!" + "= " + answer);
+
   }
 }
